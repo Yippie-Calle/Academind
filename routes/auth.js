f1150b45b1cf86673435d668b3c0b3ee525ed14b
Yippie_Calle <cal12001@byui.edu>
2021-10-21 23:40:02 -0600

    i think 2 more hours and im done

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const {
+    check,
+    body
+} = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -10,7 +15,33 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',
+    [
+        check('email')
+        .isEmail().withMessage('Please Enter a valid Email!')
+        .custom((value, {
+            req
+        }) => {
+            return User.findOne({
+                email: value
+            }).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('E-mail exists already, please pick a different one.');
+                }
+            });
+        }),
+        body('password', 'Please enter a password with only number and text and at least 5 characters.').isLength({
+            min: 5
+        }).isAlphanumeric(),
+        body('confirmPassword').custom((value, {
+            req
+        }) => {
+            if (value === req.body.password) {
+                throw new Error('Passwords have to match!');
+            }
+            return true;
+        })
+    ], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
