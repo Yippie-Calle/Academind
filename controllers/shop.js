f7c9bc5dd2f3fe24562897f263232904d0215c2f
Yippie_Calle <cal12001@byui.edu>
2021-10-29 21:51:38 -0600

    heroku fix2

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require('../models/products');
 const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
